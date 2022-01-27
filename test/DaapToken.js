eacc45b0574fc7a8840d8d669ff17f7d403a1247
Minh Phan <minh.phan@enouvo.com>
2022-01-27 22:23:57 +0700

    add transfer function and transfer event

--- test/DaapToken.js
+++ test/DaapToken.js
@@ -36,4 +36,57 @@ contract("DappToken", (accounts) => {
         assert.equal(adminBalance.toNumber(), 1000000, "Success");
       });
   });
+
+  it("Transfers token ownership", () => {
+    return DappToken.deployed()
+      .then((instance) => {
+        tokenInstance = instance;
+        return tokenInstance.transfer.call(accounts[1], 999999999999);
+      })
+      .then(assert.fail)
+      .catch((err) => {
+        assert(
+          err.message.indexOf("revert") >= 0,
+          "error msg must contain revert"
+        );
+
+        return tokenInstance.transfer.call(accounts[1], 250000, {
+          from: accounts[0]
+        });
+      })
+      .then((success) => {
+        assert.equal(success, true, "Success");
+        return tokenInstance.transfer(accounts[1], 250000, {
+          from: accounts[0]
+        });
+      })
+      .then((receipt) => {
+        assert.equal(receipt.logs.length, 1, "Trigger one event");
+        assert.equal(
+          receipt.logs[0].event,
+          "Transfer",
+          "Should be Transfer event"
+        );
+        assert.equal(receipt.logs[0].args._from, accounts[0], "Success");
+        assert.equal(receipt.logs[0].args._to, accounts[1], "Success");
+        assert.equal(receipt.logs[0].args._value, 250000, "Success");
+
+        return tokenInstance.balanceOf(accounts[1]);
+      })
+      .then((balance) => {
+        assert.equal(
+          balance.toNumber(),
+          250000,
+          "Add amount to the receiving account"
+        );
+        return tokenInstance.balanceOf(accounts[0]);
+      })
+      .then((balance) => {
+        assert.equal(
+          balance.toNumber(),
+          750000,
+          "Deduct the amount from the sending account"
+        );
+      });
+  });
 });
