ea8cfcdd28e154c1d9a83350ebb5484caab028df
Minh Phan <minh.phan@enouvo.com>
2022-01-27 21:58:48 +0700

    add name, symbol standard

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,9 +2,15 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract DappToken {
+    string public name = "Dapp Token";
+    string public symbol = "DAPP";
+    string public standard = "Dapp Token v1.0";
     uint256 public totalSupply;
 
-    constructor() public {
-        totalSupply = 1000000;
+    mapping(address => uint256) public balanceOf;
+
+    constructor(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
     }
 }
--- test/DaapToken.js
+++ test/DaapToken.js
@@ -1,14 +1,39 @@
 const DappToken = artifacts.require("../contracts/DappToken.sol");
 
-contract("DappToken", () => {
+contract("DappToken", (accounts) => {
+  let tokenInstance;
+
+  it("Initializes the contract with the correct values", () => {
+    return DappToken.deployed()
+      .then((instance) => {
+        tokenInstance = instance;
+        return tokenInstance.name();
+      })
+      .then((name) => {
+        assert.equal(name, "Dapp Token", "Correct Name");
+        return tokenInstance.symbol();
+      })
+      .then((symbol) => {
+        assert.equal(symbol, "DAPP", "Correct Symbol");
+        return tokenInstance.standard();
+      })
+      .then((standard) => {
+        assert.equal(standard, "Dapp Token v1.0", "Correct Standard");
+      });
+  });
+
   it("Sets the total supply upon deployment", () => {
     return DappToken.deployed()
       .then((instance) => {
-        const tokenInstance = instance;
+        tokenInstance = instance;
         return tokenInstance.totalSupply();
       })
       .then((totalSupply) => {
         assert.equal(totalSupply.toNumber(), 1000000, "Success");
+        return tokenInstance.balanceOf(accounts[0]);
+      })
+      .then((adminBalance) => {
+        assert.equal(adminBalance.toNumber(), 1000000, "Success");
       });
   });
 });
