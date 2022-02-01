0dc6d732166f4cabc0fa1e277f896b8128857bb1
Minh Phan <minh.phan@enouvo.com>
2022-02-01 12:25:31 +0700

    update website

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,36 @@
-console.log("hello");
+App = {
+  web3Provider: null,
+
+  init: function () {
+    console.log("init....");
+    return App.initWeb3();
+  },
+
+  initWeb3: function () {
+    if (typeof web3 !== undefined) {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      App.web3Provider = new Web3.providers.HttpProvider(
+        "http://localhost:7545"
+      );
+      web3 = new Web3(App.web3Provider);
+    }
+  },
+
+  initContracts: function () {
+    $.getJSON("DappTokenSale.json", function (dappTokenSale) {
+      App.contracts.DappTokenSale = TruffleContract(dappTokenSale);
+      App.contracts.DappTokenSale.setProvider(App.web3Provider);
+      App.contracts.DappTokenSale.deployed().then((instance) => {
+        console.log("Dapp token sale address", instance.address);
+      });
+    });
+  }
+};
+
+$(function () {
+  $(window).load(function () {
+    App.init();
+  });
+});
